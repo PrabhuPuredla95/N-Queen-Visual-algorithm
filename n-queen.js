da37bdf502535c6063fe36d3fd3a3057bff0f39e
PrabhuPuredla95 <prabhu.puredla3@gmail.com>
2020-01-26 14:41:45 +0530

    ui finished - working state

--- n-queen.js
+++ n-queen.js
@@ -1,25 +1,64 @@
 $(function(){
+    $("#start").hide();
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-    let isSafe = function(board, row, col,N)
+    // red line when collision
+    // icon for queen
+    // square tds
+    // message after game done
+    async function isSafe(board, row, col,N)
     {
         let i, j;
 
         /* Check this row on left side */
         for (i = 1; i < col; i++)
-            if ($("tbody").find('tr').eq(row).find('td').eq(i).text() == "Q")
+            if ($("tbody").find('tr').eq(row).find('td').eq(i).text() != "#")
+            {
+                $("tbody").find('tr').eq(row).find('td').eq(col).text("hit")
+                $("tbody").find('tr').eq(row).find('td').eq(col).addClass("blinking")
+                await sleep(1000)
+                $("tbody").find('tr').eq(row).find('td').eq(col).removeClass("blinking")
+                $("tbody").find('tr').eq(row).find('td').eq(col).text("#")
+
+                // $("tbody").find('tr').eq(row).find('td').eq(col).text("Q")
+                // for(j = i;j<=col;j++)
+                // {
+                //     $("tbody").find('tr').eq(row).find('td').eq(j).addClass("blinking")
+                // }
+                // await sleep(1610)
+                // for(j = i;j<=col;j++)
+                // {
+                //     $("tbody").find('tr').eq(row).find('td').eq(j).removeClass("blinking")
+                // }
+                // $("tbody").find('tr').eq(row).find('td').eq(col).text("#")
                 return false;
+            }
+
 
         /* Check upper diagonal on left side */
         for (i = row, j = col; i >= 0 && j >= 1; i--, j--)
-            if ($("tbody").find('tr').eq(i).find('td').eq(j).text() == "Q")
+            if ($("tbody").find('tr').eq(i).find('td').eq(j).text() != "#")
+            {
+                $("tbody").find('tr').eq(row).find('td').eq(col).text("hit")
+                $("tbody").find('tr').eq(row).find('td').eq(col).addClass("blinking")
+                await sleep(1000)
+                $("tbody").find('tr').eq(row).find('td').eq(col).removeClass("blinking")
+                $("tbody").find('tr').eq(row).find('td').eq(col).text("#")
                 return false;
+            }
 
         /* Check lower diagonal on left side */
         for (i = row, j = col; j >= 1 && i < N; i++, j--)
-            if ($("tbody").find('tr').eq(i).find('td').eq(j).text() == "Q")
+            if ($("tbody").find('tr').eq(i).find('td').eq(j).text() != "#")
+            {
+                $("tbody").find('tr').eq(row).find('td').eq(col).text("hit")
+                $("tbody").find('tr').eq(row).find('td').eq(col).addClass("blinking")
+                await sleep(1000)
+                $("tbody").find('tr').eq(row).find('td').eq(col).removeClass("blinking")
+                $("tbody").find('tr').eq(row).find('td').eq(col).text("#")
                 return false;
+            }
 
         return true;
     }
@@ -27,7 +66,7 @@ $(function(){
     {
         /* base case: If all queens are placed
           then return true */
-        await sleep(1000)
+        await sleep(1)
             if (col > N)
                 return true;
             let i;
@@ -36,12 +75,12 @@ $(function(){
             for (i = 0; i <N; i++) {
             /* Check if the queen can be placed on
               board[i][col] */
-                if (isSafe(board, i, col,N)) {
+                if (await isSafe(board, i, col,N)) {
                     /* Place this queen in board[i][col] */
                     //board[i][col] = "Q";
 
-                    $("tbody").find('tr').eq(i).find('td').eq(col).text("Q")
-
+                    //$("tbody").find('tr').eq(i).find('td').eq(col).text("Q")
+                    $("tbody").find('tr').eq(i).find('td').eq(col).html("<i class=\"chess rook icon big\"></i>")
                     /* recur to place rest of the queens */
                     if (await solveNQUtil(board, col + 1, N))
                         return true;
@@ -49,7 +88,11 @@ $(function(){
                     /* If placing queen in board[i][col]
                        doesn't lead to a solution, then
                        remove queen from board[i][col] */
-                    $("tbody").find('tr').eq(i).find('td').eq(col).text("#") // BACKTRACK
+                    $("tbody").find('tr').eq(i).find('td').eq(col).text("backtrack");
+                    $("tbody").find('tr').eq(i).find('td').eq(col).addClass("blinking")
+                    await sleep(1000)
+                    $("tbody").find('tr').eq(i).find('td').eq(col).removeClass("blinking")
+                    $("tbody").find('tr').eq(i).find('td').eq(col).html("#")
                 }
             }
 
@@ -62,7 +105,7 @@ $(function(){
 
         //alert("clic")
 
-        x = 4;
+        x = $('#size').val();
         let str="";
         let i,j;
         str = `<tr><th>  </th>`
@@ -84,6 +127,7 @@ $(function(){
             str = str + `</tr>`
             $('tbody').append(str)
         }
+        $("#start").show();
     });
 
     $('#start').click(function () {
